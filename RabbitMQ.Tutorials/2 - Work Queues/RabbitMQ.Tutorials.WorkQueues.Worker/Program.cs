7af3a64cfe4ea168e96019ea83a6fbbcab95a52f
Lucas Silva <luckkass89@gmail.com>
2021-01-24 16:21:02 -0300

    2 - Work Queues

---
+++ RabbitMQ.Tutorials/2 - Work Queues/RabbitMQ.Tutorials.WorkQueues.Worker/Program.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMQ.Tutorials.WorkQueues.Worker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("task_queue", true, false, false, null);
+
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+                Console.WriteLine(" [*] Waiting for messages.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += Consumer_Received;
+
+                channel.BasicConsume("task_queue", false, consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs ea)
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine(" [x] Received {0}", message);
+
+            int dots = message.Split(".").Length - 1;
+            Thread.Sleep(dots * 1000);
+
+            Console.WriteLine(" [x] Done");
+
+            // Note: it is possible to access the channel via
+            //       ((EventingBasicConsumer)sender).Model here
+            ((EventingBasicConsumer)sender).Model.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+        }
+    }
+}
