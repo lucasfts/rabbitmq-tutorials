5afa06c57d7deeb2437e3a796b30e6f968c36fb6
Lucas Silva <luckkass89@gmail.com>
2021-01-30 16:33:56 -0300

    4 - Routing

---
+++ RabbitMQ.Tutorials/4 - Routing/RabbitMQ.Tutorials.Routing.ReceiveLogsDirect/Program.cs
@@ -0,0 +1,54 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Tutorials.Routing.ReceiveLogsDirect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare("direct_logs", "direct");
+                var queueName = channel.QueueDeclare().QueueName;
+
+                if (args.Length < 1)
+                {
+                    Console.Error.WriteLine("Usage: {0} [info] [warning] [error]",
+                                        Environment.GetCommandLineArgs()[0]);
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                    Environment.ExitCode = 1;
+                    return;
+                }
+
+                foreach (var severity in args)
+                {
+                    channel.QueueBind(queueName, "direct_logs", severity);
+                }
+
+                Console.WriteLine(" [x] Waiting for messages.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += Consumer_Received;
+
+                channel.BasicConsume(queueName, true, consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            var body = e.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            var routingKey = e.RoutingKey;
+            Console.WriteLine(" [x] Received '{0}':'{1}'", routingKey, message);
+        }
+    }
+}
